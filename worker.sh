aca9b7079e981cf85c4fcaedb72bfc5fdf8763ba
Ozzy Scuba <ozzysp@icloud.com>
2022-10-25 03:26:29 -0300

    Update vagrant script

--- worker.sh
+++ worker.sh
@@ -1,1 +1,1 @@
-    docker swarm join --token SWMTKN-1-03lpusnfxxyzduw6784okqf7yra8p43u8h2728rfvp2m0vr398-73v5mdcph26hvmreogo4dscte 192.168.56.10:2377
+    docker swarm join --token SWMTKN-1-09mc3gf7frcf38pxdxxubzshsts5x7y62a0b84vyg20aqi6fn8-6vw56uy8772li2czfbtlgfqwe 192.168.56.10:2377
